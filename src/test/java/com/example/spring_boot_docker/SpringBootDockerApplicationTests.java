80dfed94d14123bc4cbad5f215e6cab24d0b20d8
Kristian Penaflorida <kpenaflorida@Kristians-MacBook-Pro.local>
2024-10-10 20:56:53 -0700

    initial

--- src/test/java/com/example/spring_boot_docker/SpringBootDockerApplicationTests.java
+++ src/test/java/com/example/spring_boot_docker/SpringBootDockerApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.spring_boot_docker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SpringBootDockerApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void simpleTest() {
+		assertTrue(true);
 	}
 
 }
